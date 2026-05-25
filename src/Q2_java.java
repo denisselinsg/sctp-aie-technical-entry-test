64239e5d345621d58cd702b857a8bd8516baa0ef
denisselinsg <soesoenyein@gmail.com>
2026-05-25 15:22:58 +0800

    Q2_java.java

--- src/Q2_java.java
+++ src/Q2_java.java
@@ -19,24 +19,45 @@ public class Q2_java {
 
     static ArrayList<String> inventory = new ArrayList<>();
 
-    public static void addItem(String itemName) {
-        // Add your code here
+    public void addItem(String itemName) {
+        if (inventory.contains(itemName)) {
+            System.out.println(itemName + " is already in inventory.");
+        } else {
+            inventory.add(itemName);
+            System.out.println(itemName + " added to inventory.");
+        }
     }
 
     public static void listInventory() {
         // Add your code here
+        
+
+    	if (inventory.isEmpty()) {
+        	System.out.println("Inventory is empty.");
+    	} else {
+        	System.out.println("Inventory: " + inventory);
+    	}
+
     }
 
     public static void main(String[] args) {
+         Q2_java manager = new Q2_java();
         // Task 4: Call the methods in this order and observe the output:
-        addItem("Laptop");
-        addItem("Mouse");
-        addItem("Keyboard");
-        addItem("Mouse");   // Should trigger duplicate warning
-        listInventory();
+       // addItem("Laptop");
+       // addItem("Mouse");
+       // addItem("Keyboard");
+       // addItem("Mouse");   // Should trigger duplicate warning
+       // listInventory();
 
         // Expected output:
         // Mouse is already in inventory.
         // Inventory: [Laptop, Mouse, Keyboard]
+
+        manager.addItem("Laptop");
+        manager.addItem("Mouse");
+        manager.addItem("Keyboard");
+        manager.addItem("Mouse");   // Should trigger duplicate warning
+        manager.listInventory();
+        
     }
 }
